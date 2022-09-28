7c026cadb94a87b12d2a75099a16ddf1c1a0335e
shubhoahmed <shubhoahmed03@gmail.com>
2022-09-29 03:07:49 +0600

    toast added

--- src/Components/EscapeDetails/EscapeDetails.js
+++ src/Components/EscapeDetails/EscapeDetails.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toast'
 
 const EscapeDetails = ({ totalTime, pausetime }) => {
+    const wave = () => toast('Successful 👋')
     return (
         <div className='mt-8'>
             <h1 className='text-2xl font-semibold'>Escape Details</h1>
             <div className='mt-4'>
-                <h2 className='my-8'>Total Duration : {totalTime}</h2>
+                <h2 className='my-8'>Total Duration : {totalTime}s</h2>
                 <h2 className='my-8'>Pause Time: {pausetime}s</h2>
-                <button className="btn btn-primary w-full mt-8">Activity  Complete</button>
+                <button onClick={() => wave(toast)} className="btn btn-primary w-full mt-8">
+                    <ToastContainer />
+                    Activity  Complete
+                </button>
             </div>
 
         </div>
