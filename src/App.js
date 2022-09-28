915106b5b08f3a3066476452d52e28739c186a61
shubhoahmed <shubhoahmed03@gmail.com>
2022-09-29 03:51:46 +0600

    answer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import EscapeData from './Components/EscapeData.js/EscapeData';
+import Questions from './Components/Questions/Questions';
 
 function App() {
   return (
     <div className="App">
       <EscapeData></EscapeData>
+      <Questions></Questions>
     </div>
   );
 }
--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Questions = () => {
     return (
-        <div>
-            <h1> thi hfdjhjk</h1>
+        <div className='mt-8 px-2'>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How does react work?
+                </div>
+                <div className="collapse-content">
+                    <p>React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    Difference between props and state.
+                </div>
+                <div className="collapse-content">
+                    <p>React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How we can you use useEffect?
+                </div>
+                <div className="collapse-content">
+                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                </div>
+            </div>
         </div>
     );
 };
