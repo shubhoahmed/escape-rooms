c9f2a839333ad180819c7f0ce219a6840f463a26
shubhoahmed <shubhoahmed03@gmail.com>
2022-09-29 03:39:15 +0600

    question added

--- src/Components/EscapeDetails/EscapeDetails.js
+++ src/Components/EscapeDetails/EscapeDetails.js
@@ -5,11 +5,11 @@ const EscapeDetails = ({ totalTime, pausetime }) => {
     const wave = () => toast('Successful 👋')
     return (
         <div className='mt-8'>
-            <h1 className='text-2xl font-semibold'>Escape Details</h1>
-            <div className='mt-4'>
+            <h1 className='text-2xl font-semibold px-3'>Escape Details</h1>
+            <div className='mt-4 px-3'>
                 <h2 className='my-8'>Total Duration : {totalTime}s</h2>
                 <h2 className='my-8'>Pause Time: {pausetime}s</h2>
-                <button onClick={() => wave(toast)} className="btn btn-primary w-full mt-8">
+                <button onClick={() => wave(toast)} className="btn btn-primary w-full mt-8 mb-3">
                     <ToastContainer />
                     Activity  Complete
                 </button>
--- src/Components/FinishTIme/FinisTime.js
+++ src/Components/FinishTIme/FinisTime.js
@@ -3,8 +3,8 @@ import React from 'react';
 const FinisTime = ({ pauseTimeDuration }) => {
     return (
         <div>
-            <h1 className='text-2xl mt-8 font-semibold'>Select finish time</h1>
-            <div className='mt-2 space-x-3 w-full gap-12'>
+            <h1 className='  text-2xl mt-12 mb-12 font-semibold px-3'>Select finish time</h1>
+            <div className=' bg-gray-300 py-4 rounded-md mt-2 space-x-2 w-full gap-12 px-3'>
                 <button onClick={() => pauseTimeDuration(15)} className="btn w-12 h-8 rounded-full">15s</button>
                 <button onClick={() => pauseTimeDuration(25)} className="btn w-12 h-8 rounded-full">25s</button>
                 <button onClick={() => pauseTimeDuration(35)} className="btn w-12 h-8 rounded-full">35s</button>
